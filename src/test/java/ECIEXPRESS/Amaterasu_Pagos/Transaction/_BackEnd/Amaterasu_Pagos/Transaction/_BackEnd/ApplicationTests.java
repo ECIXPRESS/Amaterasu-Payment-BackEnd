4c62d08c4913d057eeffda7cbe96176e5f8b55ce
tomasote572 <tomas.ramirez@mail.escuelaing.edu.co>
2025-11-13 17:31:49 -0500

    Repositorio con tecnologias

---
+++ src/test/java/ECIEXPRESS/Amaterasu_Pagos/Transaction/_BackEnd/Amaterasu_Pagos/Transaction/_BackEnd/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ECIEXPRESS.Amaterasu_Pagos.Transaction._BackEnd.Amaterasu_Pagos.Transaction._BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
