bf7928715c53e940094e2f05a199023b8df40eef
tomasote572 <tomas.ramirez@mail.escuelaing.edu.co>
2025-11-13 17:31:49 -0500

    Repositorio con tecnologias

---
+++ src/main/java/ECIEXPRESS/Amaterasu_Pagos/Transaction/_BackEnd/Amaterasu_Pagos/Transaction/_BackEnd/Application.java
@@ -0,0 +1,13 @@
+package ECIEXPRESS.Amaterasu_Pagos.Transaction._BackEnd.Amaterasu_Pagos.Transaction._BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
