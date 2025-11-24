b73bf2bb3131a25fbbfaf9fda175901735930f83
komi_yes <danielpalacios04@gmail.com>
2025-11-23 20:04:33 -0500

    feat: commit avance primera implementacion de pagos

--- src/main/java/ECIEXPRESS/AmaterasuPagos/Payment/BackEnd/Application/Dto/StrategyContext.java
+++ src/main/java/ECIEXPRESS/AmaterasuPagos/Payment/BackEnd/Application/Dto/StrategyContext.java
@@ -1,11 +1,10 @@
 package ECIEXPRESS.AmaterasuPagos.Payment.BackEnd.Application.Dto;
 
-import ECIEXPRESS.AmaterasuPagos.Payment.BackEnd.Application.Dto.PaymentRequests.CreatePaymentRequest;
 import ECIEXPRESS.AmaterasuPagos.Payment.BackEnd.Domain.Model.BankValidationResult;
 import ECIEXPRESS.AmaterasuPagos.Payment.BackEnd.Domain.Model.GatewayResponse;
 
 public record StrategyContext(
-        CreatePaymentRequest createPaymentRequest,
+        PaymentDto paymentDto,
         GatewayResponse gatewayResponse,
         BankValidationResult bankValidationResult
 ) {
