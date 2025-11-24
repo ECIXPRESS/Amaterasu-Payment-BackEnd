50254501950c7e25fb0c79d1aea7016b08cc830a
Elizabeth Correa <elizabeth.correa-s@mail.escuelaing.edu.co>
2025-11-24 17:04:44 -0500

    feat:primera implementacion ci

---
+++ src/main/java/ECIEXPRESS/AmaterasuPagos/Payment/BackEnd/Application/Dto/PaymentRequests/CreatePaymentRequest.java
@@ -0,0 +1,15 @@
+package ECIEXPRESS.AmaterasuPagos.Payment.BackEnd.Application.Dto.PaymentRequests;
+
+import ECIEXPRESS.AmaterasuPagos.Payment.BackEnd.Domain.Model.BankDetails;
+import ECIEXPRESS.AmaterasuPagos.Payment.BackEnd.Domain.Model.PaymentMethod;
+
+public record CreatePaymentRequest(
+        String orderId,
+        String clientId,
+        String storeId,
+        double originalAmount,
+        PaymentMethod paymentMethod,
+        BankDetails bankDetails
+        ) {
+}
+
