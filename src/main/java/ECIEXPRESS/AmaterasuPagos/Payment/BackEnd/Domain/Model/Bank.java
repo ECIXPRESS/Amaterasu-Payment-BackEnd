b94857b7bb9b1fd56ca2d24c64eaeb6218133535
komi_yes <danielpalacios04@gmail.com>
2025-11-24 20:25:54 -0500

    refactor(receipt): Update receipt creation logic for bank details
    Update PaymentMethod and Bank to include bankReceiptNumber and bankName. Adjust CreateReceiptRequest and ReceiptProviderAdapter to pass these details to the receipt microservice, supporting proper bank payment registration.

--- src/main/java/ECIEXPRESS/AmaterasuPagos/Payment/BackEnd/Domain/Model/Bank.java
+++ src/main/java/ECIEXPRESS/AmaterasuPagos/Payment/BackEnd/Domain/Model/Bank.java
@@ -12,11 +12,22 @@ public class Bank extends PaymentMethod {
     private PaymentMethodType paymentMethodType;
     private BankPaymentType bankPaymentType;
     private BankAccountType bankAccountType;
+    private String bankReceiptNumber;
     private String bankName;
 
     public PaymentMethod createPaymentMethod() {
         Bank bank = new Bank();
         bank.setPaymentMethodType(PaymentMethodType.BANK);
         return bank;
+    }
+
+    @Override
+    public String getBankReceiptNumber() {
+        return bankReceiptNumber;
+    }
+
+    @Override
+    public String getBankName() {
+        return bankName;
     }
 }
